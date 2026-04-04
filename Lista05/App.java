0246981b56d4dd0f6bcc2e6ede2420e4726b3ae2
ThomasAugustoH <thomas.a.haskel@gmail.com>
2026-04-04 20:55:29 -0300

    mostrando entrada

--- Lista05/App.java
+++ Lista05/App.java
@@ -5,6 +5,7 @@ public class App {
         Transposicao transposicao = new Transposicao();
         String textoPuro = "VAMOS ATACAR O SUL NO FINAL DESTA SEMANA";
         int parametroColunas = 7;
+        System.out.println("Entrada: " + textoPuro);
 
         String textoCifrado = transposicao.cifrar(textoPuro, parametroColunas);
         System.out.println("Texto cifrado: " + textoCifrado);
@@ -16,6 +17,7 @@ public class App {
         CercaFerroviaria cercaFerroviaria = new CercaFerroviaria();
         textoPuro = "VAMOS INVADIR O SUL AMANHA";
         int parametroTrilhos = 3;
+        System.out.println("Entrada: " + textoPuro);
 
         textoCifrado = cercaFerroviaria.cifrar(textoPuro, parametroTrilhos);
         System.out.println("Texto cifrado: " + textoCifrado);
