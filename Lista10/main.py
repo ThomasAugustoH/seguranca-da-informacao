c238d60a5e94ef5e5756ae347d42dc282a81d371
incrisvel <sjcristina9@gmail.com>
2026-05-29 20:50:03 -0300

    :construction: wip(Lista10): generate and serialize keys

--- Lista10/main.py
+++ Lista10/main.py
@@ -1,2 +1,45 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+import os
+
+USER = "c"
+
+def main():
+    if no_keys():
+        private_key, public_key = generate_keypair()
+        
+        serialize_private_key(private_key)
+        serialize_public_key(public_key)
+
+def no_keys():
+    return not os.path.exists(f"./Lista10/keys/{USER}_priv.pem") or not os.path.exists(f"./Lista10/keys/{USER}_pub.pem")
+
+def generate_keypair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def serialize_private_key(private_key):
+    with open(f"./Lista10/keys/{USER}_priv.pem", "wb") as f:
+        f.write(
+            private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+        )
+
+def serialize_public_key(public_key):
+    with open(f"./Lista10/keys/{USER}_pub.pem", "wb") as f:
+        f.write(
+            public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+        )
+
 if __name__ == "__main__":
-    pass
+    main()
